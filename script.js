3142e4659130785c66fc56799509decd7514d698
Joao Habib da Silva <contajh544@gmail.com>
2025-03-08 20:10:59 -0300

    Starting the contents

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var typed = new Typed("#Apresentation", {
-    strings:["<h1>Bem vindo ao site de ajuda de informações da <span id='FNP'>FNP</span></h1>"],
-    typeSpeed: 70,
+    strings: ["<h1>Bem vindo ao site de ajuda de informações da <span id='FNP'>FNP</span></h1>"],
+    typeSpeed: 60,
 })
 var navbar = window.document.getElementById("navbar")
 function Open() {
---
+++ script2.js
@@ -0,0 +1,4 @@
+var navbar = window.document.getElementById("navbar")
+function Open() {
+    navbar.classList.toggle("open")
+}
